d505d7f1c263f63d3dce936cdd48d80cf9bd6011
sano dany <sanodany110@gmail.com>
2026-07-21 15:38:16 +0200

    imprint header feature done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.html",             // <-- Add this line for root HTML files
+    "./src/**/*.{html,js}", // Scans all HTML and JS inside src/
+  ],
   theme: {
     extend: {},
   },
