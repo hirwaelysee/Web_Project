81ed7377164c9c11a5d36c520e6d6dcaec3ebe54
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-07-24 15:45:40 +0200

    feat: Add header on imprint and privacy_policy page In this commit:     - add header on imprint page and privacy_policy     - added a new image in assets     - modified the js files both the main and header js

--- src/js/header.js
+++ src/js/header.js
@@ -2,9 +2,22 @@ function initHeader() {
     const image = document.getElementById("menu-icon");
     const menu = document.getElementById("mobile-menu");
     const text = document.getElementById("hero-text");
+    const page = window.location.pathname.split('/').pop().toLowerCase();
 
-    console.log(text)
+    console.log(page)
     if (!image || !menu) return;
+
+    switch(page){
+        case "home.html":
+            image.src = './src/assets/small-icon.png';
+            break
+        case "privacy_policy.html":
+            image.src = './src/assets/green-small.png';
+            break
+        case "imprint.html":
+            image.src= './src/assets/green-small.png'
+            break
+    }
 
     image.addEventListener("click", () => {
 
@@ -13,6 +26,23 @@ function initHeader() {
 
         // Change the image
         if (menu.classList.contains("hidden")) {
+            switch(page){
+                case "home.html":
+                    image.src = "./src/assets/small-icon.png";
+                    header.classList.remove("mb-[120px]");
+                    text.classList.remove('hidden')
+                    break
+                case "privacy_policy.html":
+                    image.src = "./src/assets/green-small.png";
+                    header.classList.remove("mb-[120px]");
+                    text.classList.remove('hidden')
+                    break;
+                case "imprint.html":
+                    image.src = "./src/assets/green-small.png";
+                    header.classList.remove("mb-[120px]");
+                    text.classList.remove('hidden')
+                    break;
+            }
             image.src = "./src/assets/small-icon.png";
             header.classList.remove("mb-[120px]");
             text.classList.remove('hidden')
